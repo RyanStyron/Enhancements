ee330b68e38b910e435d330b990f3045907ede3b
RyanStyron <58631871+RyanStyron@users.noreply.github.com>
2021-08-04 14:34:35 -0400

    Add Enhancements.java framework.

---
+++ src/net/heliosphere/enhancements/Enhancements.java
@@ -0,0 +1,26 @@
+package net.heliosphere.enhancements;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Enhancements extends JavaPlugin {
+
+    private static Enhancements instance;
+
+    public static Enhancements getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        new Register(this);
+        Bukkit.getLogger().info("Enhancements loaded.");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("Enhancements disabled.");
+    }
+}
